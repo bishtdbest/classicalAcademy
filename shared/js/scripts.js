2d3e5fa30346ec3d5752206ecf398e0fffab2712
Lalit Singh Bisht <bishtdbest@gmail.com>
2026-01-06 22:32:15 +0530

    Update scripts.js

--- shared/js/scripts.js
+++ shared/js/scripts.js
@@ -9,13 +9,13 @@ $(function () {
   });
 
   // Handle popup open and close
-  $(document).on("click", ".popupLink, .popup-overlay, .popupCloseBtn", function (event) {
-    if ($(event.target).is(".popupLink")) {
-      $(".popup-overlay").fadeIn(200);
-    } else if ($(event.target).is(".popup-overlay, .popupCloseBtn")) {
-      $(".popup-overlay").fadeOut(200);
-    }
-  });
+//   $(document).on("click", ".popupLink, .popup-overlay, .popupCloseBtn", function (event) {
+//     if ($(event.target).is(".popupLink")) {
+//       $(".popup-overlay").fadeIn(200);
+//     } else if ($(event.target).is(".popup-overlay, .popupCloseBtn")) {
+//       $(".popup-overlay").fadeOut(200);
+//     }
+//   });
 
   // Initialize slick slider
   $(".featuredSlider").slick({
